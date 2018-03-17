608f6159db6fb5dbadd7ab430223ca746fb54700
SparkLv <Sparklv@outlook.com>
2018-03-17 20:21:33 +0800

    多页面配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     //入口文件使用对象形式，适用多页面
     entry: {
         //路径为相对于根目录
-        index: './src/index.js'
+        index: './src/index.js',
+        blog: './src/pages/blog.js'
     },
     module: {
         rules: [
@@ -29,6 +30,14 @@ module.exports = {
             favicon: path.resolve(__dirname, "../favicon.ico"),
             title: "spark's blog",
             chunks: ['index']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'static/html/blog.html',
+            template: path.resolve(__dirname, "../src/pages/blog.html"),
+            inject: "body",
+            favicon: path.resolve(__dirname, "../favicon.ico"),
+            title: "spark's blog",
+            chunks: ['blog']
         }),
     ]
 }
