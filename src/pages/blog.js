7d5f1e2cff877acecedcb8167854a919967ecce8
SparkLv <Sparklv@outlook.com>
2018-03-18 17:06:01 +0800

    微信登录

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,1 +1,8 @@
 console.log('sssssss');
+
+window.onload = function () {
+    let btn = document.getElementById('check-btn');
+    btn.onclick = function () {
+        console.log(navigator.userAgent.toLowerCase().indexOf('micromessenger'));
+    }
+}
