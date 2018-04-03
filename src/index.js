64e3c4764efe25db9e20021acd0f34466e95ae3e
SparkLv <Sparklv@outlook.com>
2018-04-03 21:18:09 +0800

    卡片

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './index.scss';
 import '../common/common.scss';
+
+function colorTags() {
+    const tags = document.getElementsByClassName('bar-tag');
+    const tagsArr = Array.prototype.slice.call(tags);
+    tagsArr.forEach(function (item) {
+        if (item.firstChild.innerHTML === "Webpack") {
+            item.style.backgroundColor = 'skyblue';
+        }
+    })
+}
+
+window.onload = function () {
+    colorTags();
+}
