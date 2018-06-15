f0acaffc3c709e10a5d349241bc3aa3a812021c9
lvwenbin@youyuwo.com <lvwenbin@youyuwo.com>
2018-06-15 16:38:21 +0800

    init

---
+++ pages/blog/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <span>{{text}}</span>
+        <button @click="changeText">切换</button>
+    </div>
+</template>
+<script>
+export default {
+  asyncData({ query }) {
+    return { x: query };
+  },
+  data() {
+    return {
+      text: "aaaaa"
+    };
+  },
+  mounted() {
+    console.log(this.x);
+  },
+  methods: {
+    changeText() {
+      if (this.text === "aaaaa") {
+        this.text = "bbbb";
+      } else {
+        this.text = "aaaaa";
+      }
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
